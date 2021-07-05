6384c65115a9aa15899901e144fba236a2171752
Ednardo Paz <eds_paz@hotmail.com>
2021-07-05 10:15:17 -0300

    projeto finalizado

--- dogs/src/components/login/LoginForm.js
+++ dogs/src/components/login/LoginForm.js
@@ -40,7 +40,7 @@ const LoginForm = () => {
         ) : (
           <Buttons>Entrar</Buttons>
         )}
-        <Error error={error} />
+        <Error error={error && 'Dados incorretos.'} />
       </form>
       <Link className={styles.perdeu} to="/login/perdeu">
         Perdeu a Senha?
